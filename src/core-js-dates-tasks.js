fbab55c67f89c92f7dc222c951e7ab540929eb10
Angelina Bezik <angelina1337bz@gmail.com>
2024-03-20 00:57:34 +0300

    fix: getTime

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -33,7 +33,7 @@ function dateToTimestamp(date) {
  * Date(2015, 10, 20, 23, 15, 1) => '23:15:01'
  */
 function getTime(date) {
-  return date.toLocaleTimeString();
+  return `${String(date.getHours()).padStart(2, '0')}:${String(date.getMinutes()).padStart(2, '0')}:${String(date.getSeconds()).padStart(2, '0')}`;
 }
 
 /**
